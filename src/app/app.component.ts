f7208c2d6712522b4f7e16256c94cdd0213f1966
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2023-02-19 16:27:51 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'qr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+
+  constructor(private http: HttpClient) { }
+  
+  form = new FormGroup({
+    qrType: new FormControl('', [Validators.required]),
+    wifiQR: new FormGroup({
+      ssid: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      hidden: new FormControl(false, [Validators.required]),
+      encryption: new FormControl('WPA', [Validators.required])
+    }),
+    urlQR: new FormGroup({
+      url: new FormControl('', [Validators.required, Validators.pattern('http(s)*://.+')]),
+    }),
+    textQR: new FormGroup({
+      text: new FormControl('', [Validators.required])
+    }),
+  })
+
+  qrFileName: string = '';
+
+  ngOnInit(): void {
+    this.form.get('qrType')?.valueChanges.subscribe((value) => {
+      this.form.get(value as string)?.reset();
+    });
+  }
+
+  onSubmit(form: string | null | undefined) {
+    console.log(form);
+    if (this.form.get(form as string)) {
+      const reqBody = this.form.get(form as string)?.value;
+      reqBody.qrType = form;
+      this.http.post('http://localhost:3000/get-qr', reqBody).subscribe({
+        next: (res: any) => {
+          console.log(res);
+          this.qrFileName = res.fileName as string;
+        },
+        error: (err) => {
+        console.log(err);
+        }
+      });
+    }
+  }
+
 }
